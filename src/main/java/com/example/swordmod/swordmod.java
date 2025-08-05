0f68fc3188392237db913edfba2219279e6b48ae
Divyanshproo <Divyansh10298@gmail.com>
2025-08-05 18:32:53 +0530

    Initial commit

---
+++ src/main/java/com/example/swordmod/swordmod.java
@@ -0,0 +1,27 @@
+package com.example.swordmod;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterials;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class SwordMod implements ModInitializer {
+
+    // This is your custom sword item
+    public static final Item CUSTOM_SWORD = new SwordItem(
+            ToolMaterials.DIAMOND, // Base tool material (you can change later)
+            6, // Attack damage bonus
+            -2.4F, // Attack speed
+            new Item.Settings()
+    );
+
+    @Override
+    public void onInitialize() {
+        // Register the sword
+        Registry.register(Registry.ITEM, new Identifier("swordmod", "custom_sword"), CUSTOM_SWORD);
+    }
+}
